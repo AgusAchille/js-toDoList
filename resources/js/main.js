915e7e80711c520a2ac96c30266875fc18b9b785
Agustín Achille <agustin.a.fr@gmail.com>
2019-02-05 15:03:15 -0300

    added items and icons

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+//Usuario haciendo click en el add button
+//si hay texto en el campo de texto entonces se lo agrega a la lista
+document.getElementById('add').addEventListener('click', function(){
+    let value = document.getElementById('item').value;
+    
+    //Si el valor existe (si tiene algo escrito)
+    if(value==true)
+        console.log(value)
+
+});
